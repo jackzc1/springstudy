fa6a837cae00c9837f4f9cac0f1d754181ddbe35
zc <zc@qq.com>
2020-02-10 16:34:02 +0800

    多配置文件的2中方法

--- src/test/java/com/soft/test/Demo2.java
+++ src/test/java/com/soft/test/Demo2.java
@@ -1,5 +1,6 @@
 package com.soft.test;
 
+import com.soft.dao.UserDao;
 import com.soft.service.UserService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -22,5 +23,21 @@ public class Demo2 {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
         UserService userService1 = (UserService) applicationContext.getBean("userService4");
         userService1.hello();
+    }
+
+    //多配置文件测试在主配置文件中引入另一个配置文件
+    @Test
+    public void test3() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserDao userDao2 = (UserDao) applicationContext.getBean("userDao2");
+        userDao2.findById(1);
+    }
+
+    //多配置文件测试在代码中直接加载2个配置文件
+    @Test
+    public void test4() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml","applicationContext1.xml");
+        UserDao userDao2 = (UserDao) applicationContext.getBean("userDao2");
+        userDao2.findById(1);
     }
 }
